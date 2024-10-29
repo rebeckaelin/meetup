9ca21b8ca5759f4ec6fb81c7715b38ed09482f86
rebeckaelin <rebeckaelin@hotmail.com>
2024-10-29 14:38:54 +0100

    done with get all meetups

--- server/services/utils/responses.js
+++ server/services/utils/responses.js
@@ -1,30 +1,26 @@
-
 export const sendResponse = (statusCode, message, data) => {
-    return {
-        statusCode: statusCode,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ 
-                success: true, 
-                message: message,
-                data: data
-            }
-        )
-    }
-}
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      success: true,
+      // message: message,
+      data: data,
+    }),
+  };
+};
 
 export const sendError = (statusCode, message) => {
-    return {
-        statusCode: statusCode,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(
-            { 
-                success: false, 
-                message: message 
-            }
-        )
-    }
-}
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      success: false,
+      message: message,
+    }),
+  };
+};
