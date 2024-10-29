23d1a342b516610c43a0e6021e57d0070599d5c6
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-10-29 11:01:28 +0100

    gen token verify token resonses env variable in yaml

---
+++ server/services/utils/responses.js
@@ -0,0 +1,30 @@
+
+export const sendResponse = (statusCode, message, data) => {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+                success: true, 
+                message: message,
+                data: data
+            }
+        )
+    }
+}
+
+export const sendError = (statusCode, message) => {
+    return {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+            { 
+                success: false, 
+                message: message 
+            }
+        )
+    }
+}
